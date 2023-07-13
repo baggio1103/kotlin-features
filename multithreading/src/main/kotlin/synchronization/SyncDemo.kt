e4043d5ad5f4689565d79dc2c1904b82a321d464
baggio1103 <valijanov1103@mail.ru>
2023-07-14 00:08:13 +0300

    Add example using Mutex class instance to deal with race condition

---
+++ multithreading/src/main/kotlin/synchronization/SyncDemo.kt
@@ -0,0 +1,28 @@
+package synchronization
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.apache.log4j.Logger
+
+suspend fun main() {
+
+    val mutexCounter = MutexCounter()
+
+    val log: Logger = Logger.getLogger("main")
+
+    coroutineScope {
+        launch {
+            for (i in 1 .. 10_000) {
+                mutexCounter.increment()
+            }
+        }
+        log.info("Launched first coroutine")
+        launch {
+            for (i in 1 .. 5_000) {
+                mutexCounter.increment()
+            }
+        }
+        log.info("Launched second coroutine")
+    }
+    mutexCounter.counterValue()
+}
