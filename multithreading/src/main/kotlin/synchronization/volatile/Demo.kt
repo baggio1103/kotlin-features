faa65e60cbf51c4ddd651f2a5419392875711ce7
baggio1103 <valijanov1103@mail.ru>
2023-07-14 23:58:23 +0300

    Add volatile example in Kotlin

---
+++ multithreading/src/main/kotlin/synchronization/volatile/Demo.kt
@@ -0,0 +1,24 @@
+package synchronization.volatile
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
+
+suspend fun main() {
+    val shop = Shop()
+
+    val threadOne = thread {
+        runBlocking {
+            shop.sell()
+        }
+    }
+//    delay(1000)
+    shop.isOpen = true
+//    val threadTwo = thread {
+//        runBlocking {
+//            shop.sell()
+//        }
+//    }
+    threadOne.join()
+//    threadTwo.join()
+}
