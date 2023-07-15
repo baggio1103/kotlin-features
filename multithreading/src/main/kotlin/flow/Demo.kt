0d9a5697856ebb31b3f05a35c314408222a2613e
baggio1103 <valijanov1103@mail.ru>
2023-07-15 23:59:22 +0300

    Refactor the example to demonstrate the diff between list and flow

--- multithreading/src/main/kotlin/flow/Demo.kt
+++ multithreading/src/main/kotlin/flow/Demo.kt
@@ -4,28 +4,40 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
-import kotlin.random.Random
+import java.util.*
 
-val random = Random(10)
+val random = Random()
 
 suspend fun main() {
-
     coroutineScope {
-        val users = getUsers()
-        println(
-            users.map { it.name }
-        )
+        val userFlow = getUsers()
+        val userList = getUsersAsList()
+        println("Flow: $userFlow")
+        println("List: $userList")
     }
 
 }
 
 fun getUsers(): Flow<User> = flow {
     emit(getByIdAndName(
-        random.nextInt(), "baggio"
+        random.nextInt(1000), "baggio"
+    ))
+    emit(getByIdAndName(
+        random.nextInt(1000), "dave"
+    ))
+    emit(getByIdAndName(
+        random.nextInt(1000), "chris"
+    ))
+    emit(getByIdAndName(
+        random.nextInt(1000), "wayne"
+    ))
+    emit(getByIdAndName(
+        random.nextInt(1000), "tom"
     ))
 }
+
+suspend fun getUsersAsList(): List<User> = getUsers().toList()
 
 suspend fun getByIdAndName(id: Int, name: String): User {
     delay(100)
